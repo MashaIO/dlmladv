0e6aa9273d6192baf05465af54907b8b957ebffb
Sheik <sheik@Sheiks-MacBook-Pro.local>
2019-05-15 00:23:58 +0530

    Frozen cat_or_dog

--- 1_02_1_cat_or_dog_cnn.py
+++ 1_02_1_cat_or_dog_cnn.py
@@ -19,19 +19,54 @@ import os
 # load json and create model
 from keras.models import model_from_json
 
-json_file = open('model/cat_or_dog.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
+with tf.Graph().as_default():
+    with tf.Session() as sess:
+        json_file = open('model/cat_or_dog.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
 
-model = model_from_json(loaded_model_json)
+        model = model_from_json(loaded_model_json)
 
-# load weights into new model
-model.load_weights("model/cat_or_dog.h5")
-print("Loaded model from disk")
-model.compile(optimizer='adam', 
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
+        # load weights into new model
+        model.load_weights("model/cat_or_dog.h5")
+        print("Loaded model from disk")
+        model.compile(optimizer='adam', 
+                    loss='binary_crossentropy',
+                    metrics=['accuracy'])
 
+        init = tf.global_variables_initializer()
+        sess.run(init)
+        saver = tf.train.Saver()
+        saver.save(sess, './model/cat_or_dog.ckpt')
+        tf.train.write_graph(sess.graph.as_graph_def(), '.', 'cat_or_dog.pbtxt', as_text=True)
+
+#%%
+from tensorflow.python.tools import freeze_graph
+
+freeze_graph.freeze_graph('./model/cat_or_dog.pbtxt', "", False,
+                        "./model/cat_or_dog.ckpt", "dense_2/Sigmoid",
+                        "save/restore_all", "save/Const:0",
+                        "./model/frozen_cat_or_dog.pb", True, "")
+
+#%%
+from tensorflow.python.tools import optimize_for_inference_lib
+inputGraph = tf.GraphDef()
+with tf.gfile.Open('./model/frozen_cat_or_dog.pb', "rb") as f:
+    data2read = f.read()
+    inputGraph.ParseFromString(data2read)
+
+outputGraph = optimize_for_inference_lib.optimize_for_inference(
+                inputGraph,
+                ["conv2d_1_input"],
+                ["dense_2/Sigmoid"],
+                tf.int32.as_datatype_enum)
+f = tf.gfile.FastGFile('./model/opt_cat_or_dog.pb', "w")
+f.write(outputGraph.SerializeToString())
+
+#%%
+gf = tf.GraphDef()
+gf.ParseFromString(open('./model/frozen_cat_or_dog.pb','rb').read()) 
+[n.name + '=>' +  n.op for n in gf.node if n.op in ( 'Softmax','Placeholder')]      
 #%%
 from keras.preprocessing import image
 input_size = (128, 128)
@@ -49,8 +84,9 @@ else:
     
 print(prediction)     
 
+
 #%%
-def save(model, filename):
+def save_invalid_pb(model, filename):
     # First freeze the graph and remove training nodes.    
 
     output_names = model.output.op.name
@@ -68,4 +104,4 @@ def save(model, filename):
         ofile.write(frozen_graph.SerializeToString())
 
 #%%
-save(model, filename="model/cat_or_dog.pb")
+#save(model, filename="model/cat_or_dog.pb")
